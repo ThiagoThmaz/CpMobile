ce4bffdbcf62ac3487bd85dd6ba49483e764b5fc
ThiagoThmaz <kasuma17satto@gmail.com>
2025-04-15 19:13:43 -0300

    Feat: Adicionando Drawer

--- MultiVida/app/pagInicial.tsx
+++ MultiVida/app/pagInicial.tsx
@@ -1,9 +1,9 @@
-import { View } from "react-native";
+import { View, Text} from "react-native";
 
 export default function PagInicial(){
     return(
         <View>
-        
+           <Text>Pagina Inicial</Text>
         </View>
     )
 }
